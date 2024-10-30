8050c39e8ecc5895399f1f9191c150c7d7dc4360
Dan Li <98559292+dlkency@users.noreply.github.com>
2024-10-30 10:27:02 -0400

    update

--- climate_ensemble/sbatch_single_longleaf.sh
+++ climate_ensemble/sbatch_single_longleaf.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem=8000
 #SBATCH --ntasks=1
 #SBATCH --job-name=namehere
-#SBATCH --output=job/out_%j.out
-#SBATCH --error=job/err_%j.err
+#SBATCH --output=job_status/out_%j.out
+#SBATCH --error=job_status/err_%j.err
 
 module load python/3.8.8
 source ../.venv_calfews_longleaf/bin/activate
@@ -13,7 +13,7 @@ source ../.venv_calfews_longleaf/bin/activate
 label=$1
 results=$2
 
-sed 's/sourcehere/'$label'/' runtime_params_climate_tmp.ini > runtime_params.ini
+sed 's/sourcehere/'$label'/' climate_ensemble/runtime_params_climate_tmp.ini > runtime_params.ini
 results_base='CALFEWS_results/'
 mkdir ${results_base}${results}
 cp runtime_params.ini ${results_base}${results}
